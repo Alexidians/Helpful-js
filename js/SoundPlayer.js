71eacac8297d796172b11697ba5cafd3a7a61331
Alexidians <142623358+Alexidians@users.noreply.github.com>
2023-08-28 20:55:26 +0300

    Delete SoundPlayer.js

--- js/SoundPlayer.js
+++
@@ -1,8 +0,0 @@
-//works with any sound file supported by the browser
-
-function PlaySound(url) {
- var SoundFrame = document.createElement("iframe")
- SoundFrame.src = url
- SoundFrame.style.display = "none"
- document.body.appendChild(SoundFrame)
-}
