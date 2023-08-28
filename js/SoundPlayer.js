3f2cfaa901b2382e6f066ec05b0b078798d561e7
Alexidians <142623358+Alexidians@users.noreply.github.com>
2023-08-28 20:51:48 +0300

    Update SoundPlayer.js

--- js/SoundPlayer.js
+++ js/SoundPlayer.js
@@ -3,6 +3,6 @@
 function PlaySound(url) {
  var SoundFrame = document.createElement("iframe")
  SoundFrame.src = url
- SoundFrame.style.display = none;
+ SoundFrame.style.display = "none"
  document.body.appendChild(SoundFrame)
 }
