4c54c57c19c7832f82b5dbcbb7f9fd9364231032
Alex <142623358+Alexidians@users.noreply.github.com>
2023-09-09 14:19:31 +0300

    Create sound-player.js

---
+++ js/sound-player.js
@@ -0,0 +1,10 @@
+function playSound(url) {
+ var soundElem = document.createElement("audio autoplay")
+ soundElem.style.display = "none"
+ var soundSource = document.createElement("source")
+ soundSource.src = url
+ var splitSoundUrl = url.split(".")
+ soundSource.type = "audio/" + splitSoundUrl[splitSoundUrl.length]
+ soundElem.appendChild(soundSource)
+ document.body.appendChild(soundElem)
+}
