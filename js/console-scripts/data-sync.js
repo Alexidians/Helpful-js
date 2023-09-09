ccf35f870aa3a2b0ed5c4250aad6d266ecc5d021
Alex <142623358+Alexidians@users.noreply.github.com>
2023-09-09 15:32:45 +0300

    Update data-sync.js

--- js/console-scripts/data-sync.js
+++ js/console-scripts/data-sync.js
@@ -1,12 +1,12 @@
 //run this script in the page were you want to sync the data from
 /*The Script is:
 var targetWeb = open("url where to paste the data")
-var copyedLocalStorage = localStorage
-targetWeb.eval("localStorage = " + copyedLocalStorage)
-var copyedCookies = document.cookie
-targetWeb.eval("document.cookie = " + copyedCookies)
-var copyedSessionStorage = sessionStorage
-targetWeb.eval("sessionStorage = " + copyedSessionStorage)
+var copyedData = localStorage
+targetWeb.eval("localStorage = " + copyedData)
+var copyedData = document.cookie
+targetWeb.eval("document.cookie = " + copyedData)
+var copyedData = sessionStorage
+targetWeb.eval("sessionStorage = " + copyedData)
 targetWeb.close()
 console.debug("Data Synhronized Sucessfully")
 */
